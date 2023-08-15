36d2c3a79e7523cd6f68ecd62a2d9fcdbd61c752
greenMakaroni <mrtn.dawid@gmail.com>
2023-08-16 00:04:20 +0100

    finished rating component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,24 @@
 
 const Button = (props) => {
+
   return (
-  <button onClick={() => props.setRating(props.num)} className="flex justify-center items-center w-[40px] h-[40px] bg-[#252d37] rounded-[50%]">
-    <p className="text-[#959eac] text-[15px]">{props.num}</p>
+  <button 
+    onClick={() => props.setRating(props.num)} 
+    className={
+        ` text-[#959eac] 
+        flex 
+        justify-center 
+        items-center 
+        w-[40px] 
+        h-[40px] 
+        bg-[#252d37] 
+        rounded-[50%]
+        duration-300
+        font-bold
+        ${props.rating == props.num ? "bg-[#fb7413] text-[white]" : "hover:text-[white]  hover:bg-[#959eac] "}`
+    }>
+
+    <p className="text-[15px]">{props.num}</p>
   </button>
   )
 }
--- src/components/Rate.jsx
+++ src/components/Rate.jsx
@@ -4,25 +4,37 @@ import Button from "./Button"
 const Rate = () => {
   const [rating, setRating] = useState(0)
   return (
-    <div className="flex flex-col justify-start w-[300px] p-[20px] bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]  from-[#252d37] to-[#121417] rounded-xl">
+    <div className="flex flex-col justify-start w-[340px] p-[25px] bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]  from-[#252d37] to-[#121417] from-30% to-100% rounded-3xl">
       <div className="mb-[15px] flex justify-center items-center w-[40px] h-[40px] bg-[#252d37] rounded-[50%]">
-        <img src="./public/images/icon-star.svg" alt="star-icon" />
+        <img className="scale-90" src="./public/images/icon-star.svg" alt="star-icon" />
       </div>
-      <h1 className="text-[white] "> How did we do?</h1>
-      <p className="text-[15px] text-[#959eac]"> Please let us know how we did with your support request. All feedback
+      <h1 className="text-[white] font-['Overpass'] font-normal text-[1.6rem] mb-[5px]"> How did we do?</h1>
+      <p className="text-[14px] text-[#959eac]"> Please let us know how we did with your support request. All feedback
         is appreciated to help us improve our offering!
       </p>
-      <div className="pt-[20px] pb-[20px] flex flex-row justify-between">
+      <div className="pt-[30px] pb-[25px] flex flex-row justify-between">
         <Button num={1} rating={rating} setRating={setRating} />
         <Button num={2} rating={rating} setRating={setRating} />
         <Button num={3} rating={rating} setRating={setRating} />
         <Button num={4} rating={rating} setRating={setRating} />
         <Button num={5} rating={rating} setRating={setRating} />
       </div>
-      <button className="text-[white] mb-[5px] rounded-3xl bg-[#fb7413] p-[7px]">
-        Submit
+      <button className="
+        text-[white] 
+        mb-[5px] 
+        rounded-3xl 
+        bg-[#fb7413] 
+        p-[12px] 
+        hover:bg-[white] 
+        hover:text-[#fb7413]
+        duration-300
+        font-['Overpass']
+        text-[14px]
+        tracking-widest
+        font-bold
+        ">
+        SUBMIT
       </button>
-
     </div>
   )
 }
