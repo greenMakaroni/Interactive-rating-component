ef678b7b5d8c982e59d04ebf944b52f002fe8d0c
greenMakaroni <mrtn.dawid@gmail.com>
2023-08-16 02:01:59 +0100

    vite config to include svg in a build

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/Interactive-rating-component/',
   plugins: [react()],
+  assetsInclude: ['**/*.gltf', '**/*.glb', '**/*.jpg', '**/*.svg'],
 })
